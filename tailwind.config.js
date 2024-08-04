c9afa2311c17c8d2235996ac877bdbb523c752c9
Petter Charles Stellberg Redfern <petter.charles@redfern.no>
2024-08-04 20:20:32 +0200

    Install Laravel Breeze

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
+
     theme: {
         extend: {
             fontFamily: {
@@ -40,6 +44,6 @@ export default {
             }
         },
     },
-    plugins: [],
-}
 
+    plugins: [forms],
+};
