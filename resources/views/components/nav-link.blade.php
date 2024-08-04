7ee0a2fbcd3d17062052851b4c115f41827b77e6
Petter Charles Stellberg Redfern <petter.charles@redfern.no>
2024-08-04 21:25:25 +0200

    Restyle nav link

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,8 +1,8 @@
 @props(['active' => false, 'button' => false])
 @php
-    $classes = 'hover:underline font-medium text-eggplant-950';
-    if ($active) $classes .= ' underline';
-    if ($button) $classes .= ' bg-eggplant-950 text-white px-3 py-1 rounded';
+    $classes = 'border-b-2 hover:border-eggplant-900 border-b-transparent transition duration-200 font-medium text-eggplant-950';
+    if ($active) $classes .= ' !border-eggplant-900';
+    if ($button) $classes .= ' bg-eggplant-950 hover:bg-eggplant-800 text-white px-3 py-1 rounded-lg border-none';
 @endphp
 
 <a {{$attributes(['class' => $classes])}}>{{$slot}}</a>
